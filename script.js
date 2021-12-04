c619a91fb11ec0518bbce97f6bd4b0e4fa72fd57
lovroledic <72972117+lovroledic@users.noreply.github.com>
2021-12-04 21:27:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
+const content = document.querySelector(".content");
+const eContainer = document.querySelector(".error-container");
+const eContainerMsg = document.querySelector(".error-container__message");
 const inName = document.querySelector("#name");
 const inDate = document.querySelector("#date");
 
 document.getElementById("button--add").onclick = function() {
-    var name = inName.value;
-    var date = new Date(inDate.value);
-    var day = date.getDate();
-    var month = date.getMonth();
-    var year = date.getYear();
-    //alert(name + " " + day + "." + month + "." + year + ".");
-    alert(date.getYear())
+    var date = new Date(inDate.value);  
+    var x = inName.value == null || inName.value == "";
+    var y = date == "Invalid Date" || date == null || date == NaN;
+    if (x || y) {
+        eContainer.classList.remove("hidden");
+        eContainerMsg.innerHTML = (x && y) ? "You must enter a name and deadline before creating a task." : 
+        x ? "You must enter a name for your task." : "You must enter a deadline.";
+    }
+    else {
+        var task = {name: inName.value, day: date.getDate(), month: date.getMonth() + 1, year: date.getFullYear()};
+        newTask(task);
+    }
+}
+
+inName.oninput = function() {
+    eContainer.classList.add("hidden");
+}
+inDate.oninput = function() {
+    eContainer.classList.add("hidden");
+}
+
+function newTask(task) {
+    var tContainer = document.querySelector("#template").cloneNode(true);
+    tContainer.removeAttribute("id");
+    tContainer.querySelector(".task__name").innerHTML = task.name;
+    tContainer.querySelector(".task__deadline").innerHTML = task.day + "." + task.month + "." + task.year + ".";
+    content.appendChild(tContainer);
 }
