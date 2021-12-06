36060cd9e1434238ca6ef373279428482a62976a
lovroledic <72972117+lovroledic@users.noreply.github.com>
2021-12-06 13:04:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.getElementById("button--add").onclick = function() {
         x ? "You must enter a name for your task." : "You must enter a deadline.";
     }
     else {
-        var task = {name: inName.value, day: date.getDate(), month: date.getMonth() + 1, year: date.getFullYear()};
+        var task = {name: inName.value, day: date.getDate().length === 1 ? "0" + date.getDate() : date.getDate(), month: (date.getMonth() + 1).length === 1 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1), year: date.getFullYear()};
         newTask(task);
     }
 }
@@ -29,10 +29,59 @@ inDate.oninput = function() {
 function newTask(task) {
     var tContainer = document.querySelector("#template").cloneNode(true);
     tContainer.removeAttribute("id");
+    tContainer.classList.remove("hidden");
     tContainer.querySelector(".task__name").innerHTML = task.name;
     tContainer.querySelector(".task__deadline").innerHTML = task.day + "." + task.month + "." + task.year + ".";
     content.appendChild(tContainer);
+    inName.value = "";
+    inDate.value = "";
 }
-function delete(tContainer) {
+function deleteTask(btnDelete) {
+    var tContainer = btnDelete.parentNode.parentNode;
     tContainer.parentNode.removeChild(tContainer);    
 }
+function editTask(btnEdit) {
+    btnEdit.setAttribute("onclick", "saveTask(this)");
+    btnEdit.innerHTML = "SAVE";
+    var tContainer = btnEdit.parentNode.parentNode;
+    var currName = tContainer.querySelector(".task__name");
+    var currDeadline = tContainer.querySelector(".task__deadline");
+    var editInput = tContainer.querySelector(".task__name--edit");
+    var editDeadline = tContainer.querySelector(".task__deadline--edit");
+    currName.classList.add("hidden");
+    currDeadline.classList.add("hidden");
+    editInput.classList.remove("hidden");
+    editDeadline.classList.remove("hidden");
+
+    editInput.value = currName.innerHTML;
+    var date = currDeadline.innerHTML;
+    var day = date.substring(0, date.indexOf("."));
+    //day = day.length === 1 ? "0" + day : day;
+    date = date.substring(date.indexOf(".") + 1);
+    var month = date.substring(0, date.indexOf("."));
+    //month = month.length === 1 ? "0" + month : month;
+    date = date.substring(date.indexOf(".") + 1);
+    var year = date.substring(0, date.indexOf("."));
+    editDeadline.value = year + "-" + month + "-" + day;
+}
+function saveTask(btnSave) {
+    btnSave.setAttribute("onclick", "editTask(this)");
+    btnSave.innerHTML = "EDIT";
+    var tContainer = btnSave.parentNode.parentNode;
+    var currName = tContainer.querySelector(".task__name");
+    var currDeadline = tContainer.querySelector(".task__deadline");
+    var editInput = tContainer.querySelector(".task__name--edit");
+    var editDeadline = tContainer.querySelector(".task__deadline--edit");
+    editInput.classList.add("hidden");
+    editDeadline.classList.add("hidden");
+    currName.classList.remove("hidden");
+    currDeadline.classList.remove("hidden");
+
+    var date = new Date(editDeadline.value);
+    var newTask = {name: editInput.value, day: date.getDate(), month: date.getMonth() + 1, year: date.getFullYear()};
+
+    currName.innerHTML = newTask.name;
+    currDeadline.innerHTML = newTask.day + "." + newTask.month + "." + newTask.year + ".";
+}
+
+// NAPRAVI FUNKCIJE ZA getDay, getMonth, getYear...
